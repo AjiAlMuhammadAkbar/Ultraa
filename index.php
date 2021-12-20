1becc6fa6bc73429a1d346ca8ce1043255b9e231
AjiAlMuhammadAkbar <96413311+AjiAlMuhammadAkbar@users.noreply.github.com>
2021-12-20 19:00:10 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+<body>
+<center>
+<table border="4">
+<th>Aritmatika</th>
+<form method="post" action="penjumlahan.php">
+<tr><td><input type="submit" value=" Penjumlahan"></tr></td></form> 
+<form method="post" action="pengurangan.php">
+<tr><td><input type="submit" value="Pengurangan"></tr></td></form> 
+<form method="post" action="pembagian.php">
+<tr><td><input type="submit" value="   Pembagian   "></tr></td></form> 
+<form method="post" action="perkalian.php">
+<tr><td><input type="submit" value="     Perkalian     "></tr></td></form> 
+</body>
+</html>
---
+++ penjumlahan.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+<h1> PENJUMLAHAN </h1>
+<form method="post" action="hasil penjumlahan.php">
+Nilai 1 = <input type="number" name="a" value="<?php echo $_POST["b"] ?>" required><br>
+Nilai 2 = <input type="number" name="b" value="<?php echo $_POST["b"] ?>" required><br>
+<br><input type="submit" value="hasil">
+</form>
+</body>
+</html>
